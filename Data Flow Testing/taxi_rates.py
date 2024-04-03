9453e43ab708a796f04f41087f6117a641c14297
tenmo2003 <tenmo2003@gmail.com>
2024-04-03 19:03:15 +0700

    added tests for data flow testing week

---
+++ Data Flow Testing/taxi_rates.py
@@ -0,0 +1,32 @@
+price_ranges = [1, 24, float('inf')]
+
+price_values = [
+    [20_000, 14_000, 12_000], 
+    [20_000, 15_500, 12_500]
+]
+
+def calculate_rate(km, carType):
+    if (km < 0 or km > 500):
+        raise ValueError('Invalid distance')
+    if (carType != 1 and carType != 2):
+        raise ValueError('Invalid car type')
+    
+    value = 0
+    for i in range(len(price_ranges)):
+        if (km <= 0): 
+            break
+        value = value + price_values[carType - 1][i] * min(km, price_ranges[i])
+        km -= price_ranges[i]
+
+    return value
+        
+def main():
+    carType = int(input('Car types available: \n1 - VF5 Plus\n2 - VF e34\nChoose: '))
+    distance = float(input('Distance travelled: '))
+    try :
+        print('{:,} VND'.format(round(calculate_rate(distance, carType))))
+    except ValueError as e:
+        print(e)
+
+if __name__ == '__main__':
+    main()
---
+++ Data Flow Testing/tests.py
@@ -0,0 +1,23 @@
+import unittest
+from taxi_rates import calculate_rate
+
+class TestTaxiRates(unittest.TestCase):
+
+    def test_negative_distance(self):
+        with self.assertRaises(ValueError):
+            calculate_rate(-1, 1)
+
+    def test_invalid_car_type(self):
+        with self.assertRaises(ValueError):
+            calculate_rate(1, -1)
+
+    def test_valid_input_one_km(self):
+        rate = calculate_rate(1, 1)
+        self.assertEqual(rate, 20000)
+
+    def test_valid_input_three_km(self):
+        rate = calculate_rate(3, 1)
+        self.assertEqual(rate, 48000)
+
+if __name__ == '__main__':
+    unittest.main()
